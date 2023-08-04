7a886bf91dfeafe0c989610b5681896128e6131f
Vasyl Tkachenko <vasyltkachenko07@gmail.com>
2023-08-04 22:26:03 +0300

    changed baseURL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3002/api',
+	baseURL: 'https://mern-blog-s.vercel.app/api',
 })
 
 instance.interceptors.request.use((config) => {
-    config.headers.Authorization = window.localStorage.getItem('token')
+	config.headers.Authorization = window.localStorage.getItem('token')
 
-    return config
+	return config
 })
 
 export default instance
